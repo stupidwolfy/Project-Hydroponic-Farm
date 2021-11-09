e0cec6b2af26de4328de1eb68a8dff50faa6d97a
stupidwolfy <dsl526b@gmail.com>
2021-11-09 09:06:46 +0000

    +add vue router / mdb

--- services/frontend/src/App.vue
+++ services/frontend/src/App.vue
@@ -1,21 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo-mdb-vue-small.png">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
 <style>
-@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
-
 #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 150px;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
