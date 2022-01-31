b481781ec82da1226b0b9921bf12210e381f30bc
stupidwolfy <dsl526b@gmail.com>
2022-01-31 09:03:03 +0000

    /(backend) fix CORS invalid origin

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -51,12 +51,14 @@ if devices is None:
 app = FastAPI()
 
 origins = [
-    "(http://localhost:8080/)",
+    "http://pi-zero.local:8080",
+    "http://pi-Project.local:8080",
+    "http://raspberrypi.local:8080",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins= origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
