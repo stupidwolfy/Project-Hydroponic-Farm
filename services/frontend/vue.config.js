67a4ee74d23e96eb562573aa14863b88c320aec3
stupidwolfy <dsl526b@gmail.com>
2022-01-31 08:38:06 +0000

    /(front-end) fix invalid header error

---
+++ services/frontend/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // options...
+  devServer: {
+    port: '8080',
+    allowedHosts: [
+      'pi-zero.local',
+      'pi-Project.local',
+      'raspberrypi.local'
+    ],
+  },
+}
