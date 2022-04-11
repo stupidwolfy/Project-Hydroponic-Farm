e62013de03c5148fc5ea274d99dea4f18d878de6
stupidwolfy <dsl526b@gmail.com>
2022-04-11 16:57:20 +0700

    +(backend) add config file template

---
+++ services/backend/src/Config.py
@@ -0,0 +1,10 @@
+firebase = {
+    "apiKey": "apiKey",
+    "authDomain": "authDomain.firebaseapp.com",
+    "databaseURL": "databaseURL",
+    "storageBucket": "storageBucket.appspot.com"
+}
+
+oAuth2clientID="oAuth2clientID"
+oAuth2clientSecret="oAuth2clientSecret"
+scope = "email profile"
