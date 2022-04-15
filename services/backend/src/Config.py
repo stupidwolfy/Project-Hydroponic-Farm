ab0426368c401091aec2c0c9a65e734eb11910df
stupidwolfy <dsl526b@gmail.com>
2022-04-15 10:15:25 +0700

    +(backend) add firebase config template

---
+++ services/backend/src/Config.py
@@ -0,0 +1,10 @@
+firebase = {
+    "apiKey": "apiKey",
+    "authDomain": "project-id.firebaseapp.com",
+    "databaseURL": "https://project-id.firebasedatabase.app",
+    "storageBucket": "project-id.appspot.com"
+}
+
+oAuth2clientID="oAuth2clientID"
+oAuth2clientSecret="oAuth2clientSecret"
+scope = "email profile"
